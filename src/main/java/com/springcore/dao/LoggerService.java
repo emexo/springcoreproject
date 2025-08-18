533f44037a926fa6195a5d5d2342f4b920d086f6
emexo <info@emexotechnologies.com>
2025-08-18 06:28:29 +0530

    Added AOP example around advice

--- src/main/java/com/springcore/dao/LoggerService.java
+++ src/main/java/com/springcore/dao/LoggerService.java
@@ -2,6 +2,7 @@ package com.springcore.dao;
 
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 @Log4j2
@@ -32,7 +33,15 @@ public class LoggerService {
         log.info("Logging after method execution completed.");
     }
 
-    @Pointcut("execution(* com.springcore.dao.UserDAOImpl.*(..))")
+    //@Pointcut("execution(* com.springcore.dao.UserDAOImpl.getUserById(..))") // Define a pointcut for methods in UserDAOImpl
+   // @Pointcut("execution(* com.springcore.dao.UserDAOImpl.*(..))") //    This pointcut matches all methods in UserDAOImpl
+   // @Pointcut("execution(* UserDAOImpl.*(..))") // This pointcut matches all methods in UserDAOImpl
+    //@Pointcut("execution(public * com.springcore.dao.UserDAOImpl.*(..))") // This pointcut matches all public methods in UserDAOImpl
+    //@Pointcut("execution(public User com.springcore.dao.UserDAOImpl.*(..))") //
+   //@Pointcut("within(com.springcore.dao.*)") // This pointcut matches all methods within the com.springcore.dao package
+   // @Pointcut("within(com.springcore.dao..*)")  // This pointcut matches all methods within the com.springcore.dao package and its sub-packages
+   // @Pointcut("bean(*DAOImpl)") // // This pointcut matches all beans whose names end with "DAOImpl"
+   @Pointcut("bean(*DAOImpl && *Service)") // // This pointcut matches all beans whose names end with "DAOImpl" and "Service"
     public void pointcut() {
         // This method is intentionally left empty.
         // It serves as a placeholder for pointcut definitions if needed in the future.
@@ -51,5 +60,23 @@ public class LoggerService {
         log.error("Exception thrown in method: " + methodName);
         log.error("Exception message: " + exception.getMessage());
         log.error("Stack trace: ", exception);
+    }
+
+    @Around("pointcut()")
+    public Object aroundAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        String methodName = proceedingJoinPoint.getSignature().getName();
+        log.info("Around advice before executing method: " + methodName);
+
+        Object result;
+        try {
+            result = proceedingJoinPoint.proceed();
+            log.info("Around advice after executing method: " + methodName);
+        } catch (Throwable throwable) {
+            log.error("Exception in around advice for method: " + methodName, throwable);
+            throw throwable; // rethrow the exception
+        }
+
+        log.info("Result from around advice: " + result);
+        return result;
     }
 }
