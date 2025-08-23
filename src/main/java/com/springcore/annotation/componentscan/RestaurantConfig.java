999d0338389e00732bc07ffe3f4b95a52950977d
emexo <info@emexotechnologies.com>
2025-08-23 15:23:33 +0530

    Code Cleanup

--- src/main/java/com/springcore/annotation/componentscan/RestaurantConfig.java
+++
@@ -1,9 +0,0 @@
-package com.springcore.annotation.componentscan;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.springcore.annotation.componentscan")
-public class RestaurantConfig {
-}
--- src/main/java/com/springcore/annotation/di/Vehicle.java
+++
@@ -1,7 +0,0 @@
-package com.springcore.annotation.di;
-
-public interface Vehicle {
-    void start();
-    void stop();
-    String getVehicleType();
-}
