3498a8dc56cca925f75c8706d064d57b49515c94
emexo <info@emexotechnologies.com>
2025-08-30 09:34:28 +0530

    Added dependency injection Autowired

---
+++ src/main/java/com/springcore/annotation/di/Driver.java
@@ -0,0 +1,29 @@
+package com.springcore.annotation.di;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * Autowired
+ *  Match by Type - inject the bean by Type
+ *  Match by Name - inject the bean by Name
+ *  If there are multiple beans of the same type, use
+ *  Qualifier
+ *  primay - to give preference to one bean over the other
+ */
+@Component
+public class Driver {
+
+
+    @Qualifier("toyota")
+    @Autowired(required = false)
+    private Vehicle vehicle;
+
+    public void drive() {
+        vehicle.drive();
+    }
+
+}
---
+++ src/main/java/com/springcore/annotation/di/Vehicle.java
@@ -0,0 +1,5 @@
+package com.springcore.annotation.di;
+
+public interface Vehicle {
+    void drive();
+}
