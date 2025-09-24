a16ce3eabc2a273f8f6a539c976b091b9dcf55d6
emexo <info@emexotechnologies.com>
2025-09-24 08:19:43 +0530

    Adding dependency injection with annotation

--- src/main/java/com/springcore/annotation/di/User.java
+++ src/main/java/com/springcore/annotation/di/User.java
@@ -1,20 +1,33 @@
 package com.springcore.annotation.di;
 
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
 
 /**
  * Autowired
  *  Match by type - inject the bean by type
  *  Match by Name - inject the bean by name
  *  Match by Qualifier - inject the bean by qualifier
+ *
+ *  Inject
+ *   Match by type - inject the bean by type
+ *   Match by Name - inject the bean by name
+ *   Match by Qualifier - inject the bean by qualifier
+ *
+ *   Resource
+ *    match by Name - inject the bean by name
+ *    match by type - inject the bean by type
+ *    match by Qualifier - inject the bean by qualifier
  */
 
 @Component
 public class User {
 
 
-    @Autowired(required = false)
+    @Resource
     private Honda honda;
 
 
