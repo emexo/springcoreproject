450d06876e72bcd5807f1230c1d03ec704fee13d
emexo <info@emexotechnologies.com>
2026-01-25 22:31:37 +0530

    Added AOP before advice example

--- src/main/java/com/springcoreproject/aop/LoggingAspect.java
+++ src/main/java/com/springcoreproject/aop/LoggingAspect.java
@@ -2,8 +2,8 @@ package com.springcoreproject.aop;
 
 import lombok.extern.log4j.Log4j2;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
@@ -14,10 +14,33 @@ import org.springframework.stereotype.Component;
 @Log4j2
 public class LoggingAspect {
 
-    @Before("execution(* com.springcoreproject.aop.PassengerDAO.getPassengerByPassportNumber(..))")
+    @Before("pointcutExample()")
     public void beforeAdvice(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         String passportNumber = joinPoint.getArgs()[0].toString();
         log.info("Executing method:{} with passport number:{} " , methodName,  passportNumber);
+    }
+
+    @After("pointcutExample()")
+    public void afterAdvice(JoinPoint joinPoint){
+        String methodName = joinPoint.getSignature().getName();
+        log.info("Method:{} execution completed." , methodName);
+    }
+
+    @Pointcut("execution(* com.springcoreproject.aop.PassengerDAO.*(..))")
+    public void pointcutExample(){
+
+    }
+
+    @AfterReturning(pointcut = "pointcutExample()", returning = "result")
+    public void afterReturningAdvice(JoinPoint joinPoint, Object result){
+        String methodName = joinPoint.getSignature().getName();
+        log.info("Method:{} returned value: {}" , methodName, result);
+    }
+
+    @AfterThrowing(pointcut = "pointcutExample()", throwing = "error")
+    public void afterThrowingAdvice(JoinPoint joinPoint, Throwable error){
+        String methodName = joinPoint.getSignature().getName();
+        log.error("Method:{} threw exception: {}" , methodName, error);
     }
 }
--- src/main/java/com/springcoreproject/aop/Passenger.java
+++ src/main/java/com/springcoreproject/aop/Passenger.java
@@ -2,9 +2,11 @@ package com.springcoreproject.aop;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class Passenger {
     private String name;
     private String passportNumber;
