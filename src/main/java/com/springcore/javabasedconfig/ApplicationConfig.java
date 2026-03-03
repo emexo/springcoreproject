5b0d0b87409fb379afc24288b083d4fa7578be76
emexo <info@emexotechnologies.com>
2026-03-03 06:20:30 +0530

    Java based configuration

---
+++ src/main/java/com/springcore/javabasedconfig/ApplicationConfig.java
@@ -0,0 +1,16 @@
+package com.springcore.javabasedconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+
+@Configuration
+public class ApplicationConfig {
+
+    @Bean("date")
+    public Date getDate(){
+        Date date = new Date();
+        return date;
+    }
+}
