b86e7258edbaa7a6cb39078fe60abc779b1e2d90
emexo <info@emexotechnologies.com>
2026-03-09 06:21:04 +0530

    post construct and pre destroy

--- src/main/java/com/springcore/annotaion/scope/Employee.java
+++ src/main/java/com/springcore/annotaion/scope/Employee.java
@@ -7,9 +7,15 @@ import org.springframework.stereotype.Component;
  * 1. @Scope: This annotation is used to specify the scope of a bean in Spring. It defines how many instances of the bean will be created and how they will be shared within the application context. The most common scopes are:
  *    - singleton: This is the default scope. Only one instance of the bean will be created, and it will be shared across the entire application context.
  *    - prototype: A new instance of the bean will be created each time it is requested
+ *
+ *    - request: A new instance of the bean will be created for each HTTP request (used in web applications).
+ *    - session: A new instance of the bean will be created for each HTTP session (used in web applications).
+ *    - application: A single instance of the bean will be created for the entire web application (used in web applications).
+ *    - websocket: A new instance of the bean will be created for each WebSocket connection (used in web applications).
+ *
  */
 
-@Scope("prototype")
+@Scope("request") // This bean will be created for each HTTP request (used in web applications)
 @Component
 public class Employee {
         private String name;
