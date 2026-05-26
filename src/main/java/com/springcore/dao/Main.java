e31de10560a108dfd35329e9b6cd6ac73dace929
emexo <info@emexotechnologies.com>
2026-05-26 07:24:35 +0530

    Spring DAO

--- src/main/java/com/springcore/dao/Main.java
+++ src/main/java/com/springcore/dao/Main.java
@@ -3,19 +3,25 @@ package com.springcore.dao;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 @Log4j2
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.springcore.dao");
         UserDAOImpl userDAO = context.getBean("userDAOImpl", UserDAOImpl.class);
 
-        User user = new User();
+     /*   User user = new User();
         user.setUsername("Gary");
         user.setPassword("gary@123");
         user.setFullName("Gary Gadel");
         user.setEmail("garygadel@gmail.com");
 
         int response = userDAO.saveUser(user);
-        log.info("Response:{}", response);
+        log.info("Response:{}", response);*/
+
+        List<User> users = userDAO.getAll();
+        users.forEach(data -> log.info(data));
     }
 }
--- src/main/java/com/springcore/dao/UserDAOImpl.java
+++ src/main/java/com/springcore/dao/UserDAOImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class UserDAOImpl {
 
@@ -14,5 +16,15 @@ public class UserDAOImpl {
         String sql = "insert into public.users(id, username, password, fullname, email) values (nextval('users_id_seq'), ?,?,?,?)";
         int  response = jdbcTemplate.update(sql,user.getUsername(), user.getPassword(), user.getFullName(), user.getEmail());
         return response;
+    }
+
+    public List<User> getAll(){
+        String sql = "select * from public.users";
+       return jdbcTemplate.query(sql, new UserMapper());
+    }
+
+    public User getUserByName(String username){
+        String sql = "select * from public.users where username=?";
+        return jdbcTemplate.queryForObject(sql, new UserMapper(), username);
     }
 }
